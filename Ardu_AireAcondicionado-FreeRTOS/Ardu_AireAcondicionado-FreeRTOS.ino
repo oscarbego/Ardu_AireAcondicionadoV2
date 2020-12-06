60b2f804e8045a57ac7451cf8644cacc19da9e38
oscar <oscar.bego@gmail.com>
2020-12-06 16:29:24 -0700

    Se agregagn los botones de alta_bomba y baja_bomba

--- Ardu_AireAcondicionado-FreeRTOS/Ardu_AireAcondicionado-FreeRTOS.ino
+++ Ardu_AireAcondicionado-FreeRTOS/Ardu_AireAcondicionado-FreeRTOS.ino
@@ -1,17 +1,6 @@
 #include <Arduino.h>
 #include <Arduino_FreeRTOS.h>
-
 #include <queue.h>
-
-#include "SPI.h"
-#include "Ethernet.h"
-#include "WebServer.h"
-
-
-static uint8_t mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-static uint8_t ip[] = { 192, 168, 2, 34 };
-
-#define PREFIX ""
 
 
 struct Boton {
@@ -22,55 +11,72 @@ struct Boton {
 QueueHandle_t cola_Mensaje;
 
 
-//void TaskWs( void *pvParameters );
-
-
-//Led de encendido
-//void TaskBlink( void *pvParameters );
-
-//Lectura desde le Serial
-//void TaskRead( void *pvParameters );
-
-//Comandos
 void on_off(void *pvParameter);
-//void alta_baja(void *pvParameter);
-//void bomba(void *pvParameter);
+void alta_baja(void *pvParameter);
+void bomba(void *pvParameter);
 
-//Salida a relevadores
+void baja_bomba(void *pvParameter);
+void alta_bomba(void *pvParameter);
+
 void Actuador(void *pvParameter);
 
 
 void setup() {
 
 
-  Ethernet.begin(mac, ip);
-
    cola_Mensaje = xQueueCreate(10, // Queue length
                               sizeof(struct Boton) // Queue item size
                               );                              
   Serial.begin(9600);
-  
-  delay(3000);
-/*
+
+  Serial.println("Init");
+  delay(1500);
+
   xTaskCreate(
-    TaskWs
-    ,  "TaskWs"   
-    ,  128  
+    on_off
+    ,  "on_off"
+    ,  64  // Stack size
     ,  NULL
-    ,  4  
+    ,  1  // Priority
     ,  NULL );
-*/
-    /*
-  xTaskCreate(
-    TaskBlink
-    ,  "Blink"   // A name just for humans
-    ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
-    ,  1  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  NULL );
-*/
+
 
     xTaskCreate(
+    bomba
+    ,  "bomba"
+    ,  64  // Stack size
+    ,  NULL
+    ,  1  // Priority
+    ,  NULL );
+
+
+    xTaskCreate(
+    alta_baja
+    ,  "alta_baja"
+    ,  64  // Stack size
+    ,  NULL
+    ,  1  // Priority
+    ,  NULL );
+
+
+    xTaskCreate(
+    baja_bomba
+    ,  "baja_bomba"
+    ,  64  // Stack size
+    ,  NULL
+    ,  1  // Priority
+    ,  NULL );
+
+    xTaskCreate(
+    alta_bomba
+    ,  "alta_bomba"
+    ,  65  // Stack size
+    ,  NULL
+    ,  1  // Priority
+    ,  NULL );
+
+
+   xTaskCreate(
     Actuador
     ,  "Actuador"   // A name just for humans
     ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
@@ -78,41 +84,23 @@ void setup() {
     ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
     ,  NULL );
 
-/*
-  xTaskCreate(
-    TaskRead
-    ,  "AnalogRead"
-    ,  128  // Stack size
-    ,  NULL
-    ,  2  // Priority
-    ,  NULL );
 
-*/
+    
 
-   xTaskCreate(
-    on_off
-    ,  "on_off"
-    ,  128  // Stack size
-    ,  NULL
-    ,  1  // Priority
-    ,  NULL );
 
 }
 
 void loop()
-{
-  // Empty. Things are done in Tasks.
-}
-
-
+{}
 
 /*--------------------------------------------------*/
 /*---------------------- Tasks ---------------------*/
 /*--------------------------------------------------*/
 
-
-void on_off( void *pvParameters __attribute__((unused)) )  // This is a Task.
+void on_off( void *pvParameters )  // This is a Task.
 {
+  Serial.println("on_off"); 
+  
   struct Boton boton = { 0 };
   
   uint8_t pushButton = 2;
@@ -136,10 +124,38 @@ void on_off( void *pvParameters __attribute__((unused)) )  // This is a Task.
   }
 }
 
-/*
-void alta_baja( void *pvParameters __attribute__((unused)) )  // This is a Task.
+void bomba( void *pvParameters )  // This is a Task.
 {
+  Serial.println("bomba"); 
+  
+  struct Boton boton = { 2 };
+  
+  uint8_t pushButton = 6;
 
+  pinMode(pushButton, INPUT);
+
+  for (;;) // A Task shall never return or exit.
+  {
+    int buttonState = digitalRead(pushButton);
+
+    if ( buttonState == 1 )
+    {     
+      if (xQueueSend(cola_Mensaje, &boton, 2000 / portTICK_PERIOD_MS) != pdTRUE)
+      {
+        Serial.println("error");  
+      }
+          
+      vTaskDelay( 500 / portTICK_PERIOD_MS );
+    }
+    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
+  }
+}
+
+
+void alta_baja( void *pvParameters )  // This is a Task.
+{
+  Serial.println("alta_baja"); 
+  
   struct Boton boton = { 1 };
   
   uint8_t pushButton = 4;
@@ -151,27 +167,24 @@ void alta_baja( void *pvParameters __attribute__((unused)) )  // This is a Task.
     int buttonState = digitalRead(pushButton);
 
     if ( buttonState == 1 )
-    {
-      
+    {     
       if (xQueueSend(cola_Mensaje, &boton, 2000 / portTICK_PERIOD_MS) != pdTRUE)
       {
         Serial.println("error");  
       }
           
       vTaskDelay( 500 / portTICK_PERIOD_MS );
-
     }
-
     vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
   }
 }
-*/
 
-/*
-void bomba( void *pvParameters __attribute__((unused)) )  // This is a Task.
+
+void baja_bomba( void *pvParameters )  // This is a Task.
 {
-
-  struct Boton boton = { 2 };
+  Serial.println("baja_bomba"); 
+  
+  struct Boton boton = { 3 };
   
   uint8_t pushButton = 3;
 
@@ -182,21 +195,45 @@ void bomba( void *pvParameters __attribute__((unused)) )  // This is a Task.
     int buttonState = digitalRead(pushButton);
 
     if ( buttonState == 1 )
-    {
-      
+    {     
       if (xQueueSend(cola_Mensaje, &boton, 2000 / portTICK_PERIOD_MS) != pdTRUE)
       {
         Serial.println("error");  
       }
           
       vTaskDelay( 500 / portTICK_PERIOD_MS );
-
     }
-
     vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
   }
 }
-*/
+
+void alta_bomba( void *pvParameters )  // This is a Task.
+{
+  Serial.println("alta_bomba"); 
+  
+  struct Boton boton = { 4 };
+  
+  uint8_t pushButton = 7;
+
+  pinMode(pushButton, INPUT);
+
+  for (;;) // A Task shall never return or exit.
+  {
+    int buttonState = digitalRead(pushButton);
+
+    if ( buttonState == 1 )
+    {     
+      if (xQueueSend(cola_Mensaje, &boton, 2000 / portTICK_PERIOD_MS) != pdTRUE)
+      {
+        Serial.println("error");  
+      }
+          
+      vTaskDelay( 500 / portTICK_PERIOD_MS );
+    }
+    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
+  }
+}
+
 
 
 void Actuador(void * pvParameters) {
@@ -205,6 +242,8 @@ void Actuador(void * pvParameters) {
 
   
   pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
   
   bool bOn_Off = false;
   bool bAlta_Baja = false;
@@ -220,144 +259,70 @@ void Actuador(void * pvParameters) {
 
          if(boton.id == 0)
          {
-            Serial.println("Se apaga o prende el Aire");
-
-            //if(digitalRead(13) == LOW)            
-              digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)                          
-            //else            
-            //  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW              
+            if(digitalRead(13) == LOW){            
+              digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)  
+              Serial.println("Aire prendido");
+            }                        
+            else{            
+              digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW 
+              Serial.println("Aire Apagado");
+            }             
             
             vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second                 
          }
 
          if(boton.id == 1)
          {
-            Serial.println("Se cambia la velocidad del Aire");
+            if(digitalRead(12) == LOW){            
+              digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)  
+              Serial.println("Vel Baja");
+            }                        
+            else{            
+              digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW 
+              Serial.println("Vel Alta");
+            }             
+            
+            vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second                 
          }
          
          if(boton.id == 2)
          {
-            Serial.println("Se prende o apaga la Bomba");
+            if(digitalRead(11) == LOW){            
+              digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)  
+              Serial.println("Bomba prendida");
+            }                        
+            else{            
+              digitalWrite(11, LOW);    // turn the LED off by making the voltage LOW 
+              Serial.println("Bomba apagada");
+            }             
+            
+            vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second                 
          }
+
+         if(boton.id == 3)
+         {
+            //if(digitalRead(11) == LOW){            
+              digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level) 
+              digitalWrite(12, LOW); 
+              Serial.println("Vel Baja con Bomba prendida");
+            //}                        
+            
+            vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second                 
+         }
+
+         if(boton.id == 4)
+         {
+            //if(digitalRead(11) == LOW){            
+              digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level) 
+              digitalWrite(12, HIGH); 
+              Serial.println("Vel Alta con Bomba prendida");
+            //}                        
+            
+            vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second                 
+         }
+         
     }
   }
 
   vTaskDelay(1);
-}
-
-/*
-void helloCmd(WebServer &server, WebServer::ConnectionType type, char *, bool)
-{
-  server.httpSuccess();
-
-  if (type != WebServer::HEAD)
-  {
-    Serial.println("Se apaga o prende el Aire");
-    
-    P(helloMsg) = "<h1>Hello, World!</h1>";
-
-    server.printP(helloMsg);
-  }
-}
-
-
-
-void TaskWs(void *pvParameters)  // This is a task.
-{
-  (void) pvParameters;
-
-  Serial.println("webserver 1");
-
-  WebServer webserver(PREFIX, 80);
-
-
-  Serial.println("webserver 2");
-
-  
-  webserver.setDefaultCommand(&helloCmd);
-  
-  Serial.println("webserver 3");
-  
-  webserver.addCommand("index.html", &helloCmd);
-
-  Serial.println("webserver 4");
-  
-  webserver.begin();
- 
-  
-  for (;;) // A Task shall never return or exit.
-  {
-    //webserver.processConnection();
-
-    vTaskDelay(10);
-  }
-  
-  vTaskDelay(10);
-}
-*/
-
-/*
-void TaskBlink(void *pvParameters)  // This is a task.
-{
-  (void) pvParameters;
-
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  for (;;) // A Task shall never return or exit.
-  {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
-  }
-}
-*/
-
-void TaskRead(void *pvParameters)  // This is a task.
-{
-  (void) pvParameters;
-  
-  struct Boton boton;
-  String *b;
-  
-  Serial.println("TaskRead");
-  for (;;)
-  {
-    if (Serial.available()) 
-    {   
-      //Serial.print(Serial.readString()); 
-      *b = Serial.readString();
-      //Serial.println(*b);
-      boton.id = 0;
-      
-      if(b == "b0")
-      {
-          boton.id = 0;
-          //Serial.println("...");
-      }      
-        
-      if(b == "b1")      
-        {
-          boton.id = 1;
-          //Serial.println("---");
-        }
-
-      if(b == "b2")      
-        {
-          boton.id = 2;
-          //Serial.println("+++");
-        }
-
-
-      Serial.println(boton.id);
-      if (xQueueSend(cola_Mensaje, &boton, 2000 / portTICK_PERIOD_MS) != pdTRUE)
-      {
-        Serial.println("error");  
-      }
-          
-      vTaskDelay( 500 / portTICK_PERIOD_MS );     
-   }    
-    vTaskDelay(10);  // one tick delay (15ms) in between reads for stability
-  }
-  
 }
